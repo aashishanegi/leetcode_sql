56eb5511d4060738303330c4a071738619e9a525
Aashisha Negi <500091664@stu.upes.ac.in>
2023-06-15 18:33:26 +0530

    Article Views I
    Write an SQL query to find all the authors that viewed at least one of their own articles.
    
    Return the result table sorted by id in ascending order.

--- leetcode4.sql
+++ leetcode4.sql
@@ -1,1 +1,1 @@
-SELECT DISTINCT viewer_id AS id from Views WHERE author_id=viewer_id ORDER BY (viewer_id);
+SELECT DISTINCT author_id AS id from Views WHERE author_id=viewer_id ORDER BY (author_id);
