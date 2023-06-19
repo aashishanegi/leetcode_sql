ce31738b4642199afe0360754228161ad1063bf0
Aashisha Negi <500091664@stu.upes.ac.in>
2023-06-19 21:40:31 +0530

    Product sales analysis 1

--- leetcode10.sql
+++ leetcode10.sql
@@ -1,4 +1,3 @@
-# Write your MySQL query statement below
 select distinct P.product_name, S.year, S.price from
 
     (select distinct product_id, year, price from Sales) S inner join Product as P using (product_id);
