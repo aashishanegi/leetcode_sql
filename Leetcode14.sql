e25a2d3eaf3bafd0025d5aaae597a985f824b01a
Aashisha Negi <500091664@stu.upes.ac.in>
2024-08-04 22:47:35 +0530

    Update Leetcode14.sql

--- Leetcode14.sql
+++ Leetcode14.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 SELECT W1.id FROM Weather W1, Weather W2
 WHERE DATEDIFF(W1.recordDate,W2.recordDate) = 1 
-AND W1.temperature>W2.temperature;
+AND W1.temperature>W2.temperature
